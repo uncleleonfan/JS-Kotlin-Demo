7957286c5e91c4da81ee65b0238afe244dcfa8ca
Leon <526247082@qq.com>
2017-11-11 19:17:37 +0800

    Using DOM.

--- src/Main.kt
+++ src/Main.kt
@@ -2,11 +2,17 @@ import kotlin.browser.document
 
 fun main(args: Array<String>) {
     val root = document.getElementById("root")
-    val html = """
-    <h1>Hello</h1>
-    <button id="b1">Click Me</button>
-    """
-    root!!.innerHTML = html
-    val b1 = document.getElementById("b1");
-    b1!!.addEventListener("click", { println("clicked")})
+
+    val div = document.createElement("div")
+    val h1 = document.createElement("h1")
+    val button = document.createElement("button")
+
+    h1.textContent = "Hello World！"
+    button.textContent = "Click Me"
+    button.addEventListener("click", { println("clicked")})
+
+    div.appendChild(h1)
+    div.appendChild(button)
+    root!!.appendChild(div)
+
 }
